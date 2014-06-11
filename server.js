336f5d67a763c0c7ac39415b1bd162ff7d5de0c3
Khemraj Thapa <khyamay@gmail.com>
2014-06-11 02:34:53 +0100

    added front-end code

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var app = express();
-mongoose.connect('localhost');
+
+mongoose.connect('localhost'); 
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
@@ -22,7 +23,8 @@ var Todo = mongoose.model('Todo', {
 
 app.get('/api/todos', function(req, res){
 	Todo.find(function(err, todos){
-		if (err) res.send(err)
+		if (err) 
+			res.send(err)
 			res.json(todos);
 	});
 });
@@ -33,27 +35,35 @@ app.post('/api/todos', function(req, res){
 		text: req.body.text, 
 		done: false
 	}, function(err, todo){
-		if(err) res.send(err);
+		if(err) 
+			res.send(err);
 
-		todo.find(function(err, todos){
+		Todo.find(function(err, todos){
 			if(err) res.send(err)
 				res.json(todos);
 		});
 	});
 });
 
-app.delet('/api/todos/:tood_id', function(req, res){
+app.delete('/api/todos/:todo_id', function(req, res){
 	Todo.remove({
 		_id: req.params.todo_id
 	}, function(err, todo){
-		if (err) res.send(err);
+		if (err) 
+			res.send(err);
 
 		Todo.find(function(err, todos){
-			if(err) res.send(err)
+			if(err) 
+				res.send(err)
 				res.json(todos);
-		})
-	})
-})
+		});
+	});
+});
+
+app.get('*', function(req, res){
+	res.sendfile('./public/index.html');
+});
+
 app.listen(app.get('port'), function () {
 	console.log('Express server is listening on port ' + app.get('port'));
 
